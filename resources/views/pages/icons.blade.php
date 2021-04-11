057b163141de0b6e5d2b75af3a521c813bc215c2
Wanarut <wanarut.b@gmail.com>
2021-04-11 22:27:52 +0700

    icon iframe

--- resources/views/pages/icons.blade.php
+++ resources/views/pages/icons.blade.php
@@ -15,9 +15,16 @@
           <div class="col-md-12">
             <div class="card-body">
               <div class="iframe-container d-none d-lg-block">
-                <iframe src="https://design.google.com/icons/">
+                {{-- <iframe src="https://www.google.com/" >
                   <p>Your browser does not support iframes.</p>
+                </iframe> --}}
+                {{-- <iframe id="frame" src="https://www.esaveag.com/slcontrol/" width="100%" height="300"> --}}
+                <iframe name="myFrame" width="100%" height="300">
                 </iframe>
+                <script>
+                  window.frames["myFrame"].location = "https://www.esaveag.com/slcontrol/"
+                </script>
+
               </div>
               <div class="col-md-12 d-none d-sm-block d-md-block d-lg-none d-block d-sm-none text-center ml-auto mr-auto">
                 <h5>The icons are visible on Desktop mode inside an iframe. Since the iframe is not working on Mobile and Tablets please visit the icons on their original page on Google. Check the
