ea9b92af11a18943fd6ceecb6ba2336ecdb84c85
pornnapa <pornnapa200@gmail.com>
2021-04-14 13:05:32 +0700

    add 15 device pages

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -19,33 +19,33 @@
         <div id="group">
         <li class="menu-title text-head ">Camera</li>
         </div>
-        <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('home') }}">
+        <li class="nav-item{{ $activePage == 'camera360' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('camera360') }}">
               <i class="icon-dome-camera text-orange"></i>
               <p>{{ __('Speed Dome Camera 360°') }}</p>
           </a>
         </li>
         
-        <li class="nav-item{{ $activePage == 'table' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('table') }}">
+        <li class="nav-item{{ $activePage == 'camera_license' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('camera_license') }}">
             <i class="icon-car text-orange"></i>
               <p>{{ __('Camera Fix Lens (plate)') }}</p>
           </a>
         </li>
-        <li class="nav-item{{ $activePage == 'typography' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('typography') }}">
+        <li class="nav-item{{ $activePage == 'camera_temp' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('camera_temp') }}">
             <i class="icon-temp text-orange"></i>
               <p>{{ __('Camera Temp Monitoring') }}</p>
           </a>
         </li>
-        <li class="nav-item{{ $activePage == 'icons' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('icons') }}">
+        <li class="nav-item{{ $activePage == 'camera_face' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('camera_face') }}">
             <i class="icon-face text-orange"></i>
             <p>{{ __('Camera Fix Lens (Face)') }}</p>
           </a>
         </li>
-        <li class="nav-item{{ $activePage == 'icons1' ? ' active' : '' }}">
-          <a class="nav-link" href="/">
+        <li class="nav-item{{ $activePage == 'intercom' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('intercom') }}">
             <i class="icon-intercom text-orange"></i>
               <p>{{ __('Intercom') }}</p>
           </a>
@@ -53,20 +53,20 @@
         <div id="group">
           <li class="menu-title text-head ">Advertisement</li>
         </div>
-        <li class="nav-item{{ $activePage == 'notifications' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('notifications') }}">
+        <li class="nav-item{{ $activePage == 'ex_speaker' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('ex_speaker') }}">
             <i class="icon-bullhorn text-orange"></i>
             <p>{{ __('Exstreamer Loud Speaker') }}</p>
           </a>
         </li>
-        <li class="nav-item{{ $activePage == 'language' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('language') }}">
+        <li class="nav-item{{ $activePage == 'in_speaker' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('in_speaker') }}">
             <i class="icon-In-loud text-orange"></i>
             <p>{{ __('Instreamer Loud Speaker') }}</p>
           </a>
         </li>
-        <li class="nav-item{{ $activePage == 'language' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('language') }}">
+        <li class="nav-item{{ $activePage == 'digital_signage' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('digital_signage') }}">
             <i class="icon-Digital_Signage text-orange"></i>
             <p>{{ __('Digital Signage') }}</p>
           </a>
@@ -74,14 +74,14 @@
         <div id="group">
           <li class="menu-title text-head ">Weather</li>
         </div>
-        <li class="nav-item ">
-          <a class="nav-link " href="">
+        <li class="nav-item {{ $activePage == 'meteodata' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('meteodata') }}">
             <i class="icon-cloudy1 text-orange"></i>
             <p>{{ __('e-Save Meteodata') }}</p>
           </a>
         </li>
-        <li class="nav-item a{{ $activePage == 'upgrade2' ? ' active' : '' }}">
-          <a class="nav-link " href="">
+        <li class="nav-item {{ $activePage == 'air_transmitter' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('air_transmitter') }}">
             <i class="icon-pm text-orange" ></i>
             <p>{{ __('Air Transmitter') }}</p>
           </a>
@@ -89,14 +89,14 @@
         <div id="group">
           <li class="menu-title text-head ">Control System</li>
         </div>
-        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
-          <a class="nav-link " href="{{ route('upgrade') }}">
+        <li class="nav-item {{ $activePage == 'occupancy' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('occupancy') }}">
             <i class="icon-occupancy text-orange"></i>
             <p>{{ __('Occupancy Sensor') }}</p>
           </a>
         </li>
-        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
-          <a class="nav-link " href="{{ route('upgrade') }}">
+        <li class="nav-item {{ $activePage == 'power_socket' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('power_socket') }}">
             <i class="icon-power-plug text-orange"></i>
             <p>{{ __('Roud Power Socket') }}</p>
           </a>
@@ -104,28 +104,29 @@
         <div id="group">
           <li class="menu-title text-head ">Smart Lighting</li>
         </div>
-        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
-          <a class="nav-link " href="{{ route('upgrade') }}">
+        <li class="nav-item {{ $activePage == 'esave_dashboard' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('esave_dashboard') }}">
             <i class="icon-gauge text-orange" ></i>
             <p>{{ __('e-Save Dashboard') }}</p>
           </a>
         </li>
-        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
-          <a class="nav-link " href="{{ route('upgrade') }}">
-            <i class="icon-imergency text-orange"></i>
-            <p>{{ __('Beacon Glow Blue') }}</p>
-          </a>
-        </li>
-        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
-          <a class="nav-link " href="{{ route('upgrade') }}">
+        <li class="nav-item {{ $activePage == 'cluster_projector' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('cluster_projector') }}">
             <i class="icon-cluster_projectors text-orange"></i>
             <p>{{ __('Cluster Projectors') }}</p>
           </a>
         </li>
-        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
-          <a class="nav-link " href="{{ route('upgrade') }}">
+        <li class="nav-item {{ $activePage == 'cluster_projector_lador' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('cluster_projector_lador') }}">
             <i class="icon-cluster_projectors_ladar text-orange"></i>
             <p>{{ __('Cluster Projectors Lador ') }}</p>
+          </a>
+        </li>
+        
+        <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
+          <a class="nav-link " href="{{ route('upgrade') }}">
+            <i class="icon-imergency text-orange"></i>
+            <p>{{ __('Beacon Glow Blue') }}</p>
           </a>
         </li>
         <li class="nav-item {{ $activePage == 'upgrade' ? ' active' : '' }}">
---
+++ resources/views/pages/digital_signage.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app', ['activePage' => 'digital_signage', 'titlePage' => __('Digital Signage')])
+
+@section('content')
+<div class="content">
+  <div class="container-fluid">
+    <div class="container-fluid">
+      <div class="card card-plain">
+        <div class="card-header card-header-primary">
+            <h4 class="card-title"> 
+                <i class="icon-Digital_Signage text-orange"></i> 
+                 <b>Digital Signage</b>
+            </h4>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card-body">
+              <div class="iframe-container d-none d-lg-block">
+                {{-- <iframe src="https://www.google.com/" >
+                  <p>Your browser does not support iframes.</p>
+                </iframe> --}}
+                {{-- <iframe id="frame" src="https://www.esaveag.com/slcontrol/" width="100%" height="300"> --}}
+                <iframe name="myFrame" width="100%" height="100%">
+                </iframe>
+                <script>
+                  window.frames["myFrame"].location = "https://www.esaveag.com/slcontrol/"
+                </script>
+
+              </div>
+              <div class="col-md-12 d-none d-sm-block d-md-block d-lg-none d-block d-sm-none text-center ml-auto mr-auto">
+                <h5>The icons are visible on Desktop mode inside an iframe. Since the iframe is not working on Mobile and Tablets please visit the icons on their original page on Google. Check the
+                  
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
