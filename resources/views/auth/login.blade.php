e0e382aef05fd2172b3625ca0e23f319b9a70a0e
pornnapa <pornnapa200@gmail.com>
2021-04-15 23:23:26 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.appLog', ['class' => 'off-canvas-sidebar', 'activePage' => 'login', 'title' => __('Material Dashboard')])
 
 @section('content')
-    <div class="container ">
-        <div class="row align-items-center" >
-            <div class="col-md-4 ml-auto mr-auto mb-3 text-center" >
-                <img src="{{ asset('material/img/logo/ligman_logo_20_2c_fn_200.png') }}" widht="100%" >
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto" style="background-color:#FFC932; background-size: cover; ">
+<div class="row align-items-center" style="height:820px; ">
+    <div class="col-md-8 ml-auto mr-auto mb-3 text-center" >
+        <img src="{{ asset('material/img/logo/ligman_logo_20_2c_fn_200.png') }}" widht="100%" >
+    </div>
+    <div class="col-md-4 ml-auto mr-auto mb-3 text-center" style="height:100% ">
+        <div class="login_layout"> 
+            <div class="login_form">
                 <form class="form" method="POST" action="{{ route('login') }}">
                     @csrf
-
                     <div class="card card-login card-hidden mb-3">
-                        <div class="card-header card-header-primary text-center">
-                            <h4 class="card-title"><strong>{{ __('Login') }}</strong></h4>
-                           
+                        <div class="card-header card-header-primary text-center" style="margin-top: -30px;height:60px;">
+                            <h3 class="card-title"><strong>{{ __('Login') }}</strong></h3>
                         </div>
                         <div class="card-body">
                             <p class="card-description text-center"></p>
@@ -21,7 +20,7 @@
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">
-                                            <i class="material-icons">email</i>
+                                        <i class="material-icons">email</i>
                                         </span>
                                     </div>
                                     <input type="email" name="email" class="form-control"
@@ -29,8 +28,7 @@
                                         value="{{ old('email', 'admin@material.com') }}" required>
                                 </div>
                                 @if ($errors->has('email'))
-                                    <div id="email-error" class="error text-danger pl-3" for="email"
-                                        style="display: block;">
+                                    <div id="email-error" class="error text-danger pl-3" for="email"style="display: block;">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </div>
                                 @endif
@@ -47,16 +45,15 @@
                                         value="{{ !$errors->has('password') ? 'secret' : '' }}" required>
                                 </div>
                                 @if ($errors->has('password'))
-                                    <div id="password-error" class="error text-danger pl-3" for="password"
-                                        style="display: block;">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                    <div id="password-error" class="error text-danger pl-3" for="password"  style="display: block;">
+                                    <strong>{{ $errors->first('password') }}</strong>
                                     </div>
                                 @endif
                             </div>
                             <div class="form-check mr-auto ml-3 mt-3">
                                 <label class="form-check-label">
                                     <input class="form-check-input" type="checkbox" name="remember"
-                                        {{ old('remember') ? 'checked' : '' }}> {{ __('Remember me') }}
+                                    {{ old('remember') ? 'checked' : '' }}> {{ __('Remember me') }}
                                     <span class="form-check-sign">
                                         <span class="check"></span>
                                     </span>
@@ -78,9 +75,8 @@
                     </div>
                 </form>
                 <div class="row">
-                    <div class="col-6">
-                    </div>
-                    <div class="col-6 text-right">
+                    <div class="col-4"></div>
+                    <div class="col-8 text-right">
                         @if (Route::has('password.request'))
                             <a href="{{ route('password.request') }}" class="text-light">
                                 <small>{{ __('Forgot password?') }}</small>
@@ -91,4 +87,6 @@
             </div>
         </div>
     </div>
+</div>
+       
 @endsection
