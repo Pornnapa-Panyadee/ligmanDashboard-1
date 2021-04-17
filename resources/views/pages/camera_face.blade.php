d385bb9336523afd1fa9ad28a3e11fbdcfd9357f
Wanarut <wanarut.b@gmail.com>
2021-04-17 17:53:08 +0700

    add licence cam

--- resources/views/pages/camera_face.blade.php
+++ resources/views/pages/camera_face.blade.php
@@ -6,30 +6,24 @@
     <div class="container-fluid">
       <div class="card card-plain">
         <div class="card-header card-header-primary">
-            <h4 class="card-title"> 
-                <i class="icon-face text-orange"></i> 
-                 <b>Camera Fix Lens (Face)</b>
-            </h4>
+          <h4 class="card-title"> 
+            <i class="icon-In-loud text-orange"></i> 
+            <b>Camera Fix Lens (Face)</b>
+          </h4>
+        </div>
+        <div class="card-header card-header-primary">
+          <h4 class="card-title">First Entry</h4>
+          <p class="card-category">If you never login to "erdi" click this button 
+            <button type="button" class="btn btn-success" onclick="loginface()">Auto Login</button> only once. After this, it is not necessary.
+          </p>
         </div>
         <div class="row">
           <div class="col-md-12">
             <div class="card-body">
               <div class="iframe-container d-none d-lg-block">
-                {{-- <iframe src="https://www.google.com/" >
-                  <p>Your browser does not support iframes.</p>
-                </iframe> --}}
-                {{-- <iframe id="frame" src="https://www.esaveag.com/slcontrol/" width="100%" height="300"> --}}
-                <iframe name="myFrame" width="100%" height="100%">
-                </iframe>
-                <script>
-                  window.frames["myFrame"].location = "https://www.esaveag.com/slcontrol/"
-                </script>
-
               </div>
               <div class="col-md-12 d-none d-sm-block d-md-block d-lg-none d-block d-sm-none text-center ml-auto mr-auto">
-                <h5>The icons are visible on Desktop mode inside an iframe. Since the iframe is not working on Mobile and Tablets please visit the icons on their original page on Google. Check the
-                  
-                </h5>
+                <h5>The icons are visible on Desktop mode inside an iframe. Since the iframe is not working on Mobile and Tablets please visit the icons on their original page on Google.</h5>
               </div>
             </div>
           </div>
@@ -37,5 +31,29 @@
       </div>
     </div>
   </div>
+  <iframe name="myFrame" width="100%" height="650px"><p>Your browser does not support iframes.</p></iframe>
 </div>
+
+<script>
+  window.frames["myFrame"].location = "http://face.erdi.cmu.ac.th/";
+</script>
+
+<form name="faceForm" id="faceForm" method="POST" action="http://face.erdi.cmu.ac.th/" target="_blank"> 
+  <input type=hidden id="username" name="username" value="ligmanproject"/>
+  <input type=hidden id="password" name="password" value="123456"/>
+</form>
+
+<script>
+  (function() {
+    var url = 'http://face.erdi.cmu.ac.th/';
+    newWindow = window.open(url, "_blank", "toolbar=no,scrollbars=yes,resizable=no,top=190,left=255,width=1640,height=800");
+    // focus on the popup //
+    newWindow.focus();
+  })();
+
+  function loginface(){
+    console.log("start login");
+    document.getElementById("faceForm").submit();
+  }
+</script>
 @endsection
