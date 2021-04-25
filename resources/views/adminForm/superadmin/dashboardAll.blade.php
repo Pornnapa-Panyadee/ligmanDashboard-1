87aac83be44e03c4ca8f29ac022c4837a6e37da6
pornnapa <pornnapa200@gmail.com>
2021-04-25 13:41:37 +0700

    create dash All of superadmin

---
+++ resources/views/adminForm/superadmin/dashboardAll.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app_superadmin', ['activePage' => 'superadminlist', 'titlePage' => __('Dashboard List')])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+        <div class="col-md-10">
+           <div class="row">
+                <div class="col-md-2" >
+                    <div class="card-dashList">
+                        <div class="card-icon text-orange" >
+                            <i class="material-icons" style="font-size:120px;">account_circle</i>
+                        </div>
+                        <h4 class="text_dashList">{{__('Name User')}}</h4>
+                     </div>
+                </div>
+
+                <div class="col-md-2" >
+                    <div class="card-dashList">
+                        <div class="card-icon text-orange" >
+                            <i class="material-icons" style="font-size:120px;">account_circle</i>
+                        </div>
+                        <h4 class="text_dashList">{{__('Name User')}}</h4>
+                     </div>
+                </div>
+
+               
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+@endsection
