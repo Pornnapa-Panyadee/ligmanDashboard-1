8d34503c09c19e9e4e3fd3e453d70b8acbc63819
pornnapa <pornnapa200@gmail.com>
2021-05-03 22:15:17 +0700

    add menu profile   create location and device

--- resources/views/layouts/navbars/sidebar_admin.blade.php
+++ resources/views/layouts/navbars/sidebar_admin.blade.php
@@ -9,19 +9,37 @@
     <div class="sidebar-wrapper">
       
       <ul class="nav">
-        <li class="nav-item{{ $activePage == 'create_device' ? ' active' : '' }}">
-          <a class="nav-link" href="{{ route('adminForm.admin.create_device') }}">
-            <i class="material-icons text-orange">settings_applications</i>
-              <p>{{ __('My Device settings') }}</p>
+        <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('profile.edit') }}">
+            <i class="material-icons text-orange">person</i>
+              <p>{{ __('Profile') }}</p>
           </a>
         </li>
-        
+
         <li class="nav-item{{ $activePage == 'listadmin' ? ' active' : '' }}">
           <a class="nav-link" href="{{ route('adminForm.admin.list') }}">
-            <i class="material-icons text-orange">people</i>
+            <i class="material-icons text-orange">format_list_bulleted</i>
             <p>{{ __('List') }}</p>
           </a>
         </li>
+
+        <li class="nav-item{{ $activePage == 'create_location' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('adminForm.admin.location') }}">
+            <i class="material-icons text-orange">location_on</i>
+              <p>{{ __('Create Pole Location ') }}</p>
+          </a>
+        </li>
+
+        <li class="nav-item{{ $activePage == 'create_device' ? ' active' : '' }}">
+          <a class="nav-link" href="{{ route('adminForm.admin.create_device') }}">
+            <i class="material-icons text-orange">lightbulb</i>
+              <p>{{ __('Create Device ') }}</p>
+          </a>
+        </li>
+
+        
+        
+        
       </ul>
     </div>
   </div>
